cdda35b886f3353e1b2b576532ffe7b4cac8c33a
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-05-06 07:17:32 +0200

    feat: Export organizations

--- dump_and_convert.sh
+++ dump_and_convert.sh
@@ -6,5 +6,6 @@
 cmd=sentry --config=$SENTRY_CONFIG django dumpdata --indent 2
 mkdir -p data
 
-$cmd sentry.user | grep -v "\[WARNING\]" > data/sentry_data.json
-cat data/sentry_data.json|jq '[.[] | select(.model="sentry.user") | .model="users.user" | del(.fields.username) | del(.fields.last_active) | del(.fields.is_sentry_app) | del(.fields.last_password_change) | del(.fields.is_managed) | del(.fields.flags) | del(.fields.session_nonce) | .fields = .fields + {"created": .fields.date_joined} | del(.fields.date_joined) | del(.fields.is_password_expired)]' > data/users_user.json
+$cmd sentry.user sentry.organization | grep -v "\[WARNING\]" > data/sentry_data.json
+cat data/sentry_data.json|jq '[.[] | select(.model=="sentry.user") | .model="users.user" | del(.fields.username) | del(.fields.last_active) | del(.fields.is_sentry_app) | del(.fields.last_password_change) | del(.fields.is_managed) | del(.fields.flags) | del(.fields.session_nonce) | .fields = .fields + {"created": .fields.date_joined} | del(.fields.date_joined) | del(.fields.is_password_expired)]' > data/users_user.json
+cat data/sentry_data.json|jq '[.[] | select(.model=="sentry.organization") | .model="organizations_ext.organization" | del(.fields.status) | del(.fields.default_role) | del(.fields.flags) | .fields.created=.fields.date_added | del(.fields.date_added)]' > data/organizations_organization.json
