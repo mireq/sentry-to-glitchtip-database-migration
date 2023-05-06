f6eda8c4dc9ac1cd3b61c92656b671378bb92fd5
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-05-06 07:14:34 +0200

    feat: Dump data in single step

--- dump_and_convert.sh
+++ dump_and_convert.sh
@@ -6,5 +6,5 @@
 cmd=sentry --config=$SENTRY_CONFIG django dumpdata --indent 2
 mkdir -p data
 
-$cmd sentry.user | grep -v "\[WARNING\]" > data/sentry_user.json
-cat data/sentry_user.json|jq '[.[] | .model="users.user" | del(.fields.username) | del(.fields.last_active) | del(.fields.is_sentry_app) | del(.fields.last_password_change) | del(.fields.is_managed) | del(.fields.flags) | del(.fields.session_nonce) | .fields = .fields + {"created": .fields.date_joined} | del(.fields.date_joined) | del(.fields.is_password_expired)]' > data/users_user.json
+$cmd sentry.user | grep -v "\[WARNING\]" > data/sentry_data.json
+cat data/sentry_data.json|jq '[.[] | select(.model="sentry.user") | .model="users.user" | del(.fields.username) | del(.fields.last_active) | del(.fields.is_sentry_app) | del(.fields.last_password_change) | del(.fields.is_managed) | del(.fields.flags) | del(.fields.session_nonce) | .fields = .fields + {"created": .fields.date_joined} | del(.fields.date_joined) | del(.fields.is_password_expired)]' > data/users_user.json
